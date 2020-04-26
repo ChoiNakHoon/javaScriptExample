64eab3286d0456084e22afd53bd2d972cca82278
raccoon86 <onehub86@gmail.com>
2020-04-26 17:02:46 +0900

    여행 목록 작성하기 삭제 버튼 만들기

--- check_list.js
+++ check_list.js
@@ -2,15 +2,25 @@ const addBtn = document.querySelector("#add"),
     itemValue = document.querySelector("#item");
 
 const itemList = [];
+
 function showList() {
     var list = "<ul>";
     for (let i = 0; i < itemList.length; i++) {
-        list += "<li>" + itemList[i] + "</li>";
+        list += "<li>" + itemList[i] + "<span class='close' id="+i+">X</span></li>";
     }
 
     list += "</ul>";
 
     document.querySelector("#itemList").innerHTML = list;
+    const removeItem = document.querySelectorAll(".close"); //삭제 버튼을 변수로 저장. 배열 형태가 됨
+
+    for (let i = 0; i < removeItem.length; i++) {
+        removeItem[i].addEventListener("click", function(){
+            const itemId = this.getAttribute("id");
+            itemList.splice(itemId, 1);
+            showList();
+        });
+    }
 }
 function addList() {
     var item = itemValue.value;
